e0d97ae89b990cb8152801e56f86595c8059df86
güneş balcı <gunesbalci66@hotmail.com>
2024-02-27 13:49:56 +0300

    Bilgi Doğrulama modeli eklendi.

---
+++ Models/BilgiDoğrulama.cs
@@ -0,0 +1,11 @@
+namespace bulgarita.Models;
+
+public class BilgiDoğrulama
+{
+    public string Kimlik;
+    public string Kullanıcı;
+    public DateTime Başlangıç;
+    public DateTime Bitiş;
+    public string Bağlantı;
+    public bool Doğrulandı;
+}
