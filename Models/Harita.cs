da18a534eb986554b50df188b6f09106e6dcc73e
güneş balcı <gunesbalci66@hotmail.com>
2024-02-27 13:49:23 +0300

    Harita modeli eklendi

---
+++ Models/Harita.cs
@@ -0,0 +1,11 @@
+namespace bulgarita.Models;
+
+public class Harita
+{
+    public double EnlemDrc;
+    public double BoylamDrc;
+    public string Mevcut_İsim;
+    public string Düzenlenmiş_İsim;
+    public string Üst_Bölge;
+    public string Kimlik;
+}
