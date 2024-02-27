ad9bea445ae9cade0c324d9c519d5737b01233d7
güneş balcı <gunesbalci66@hotmail.com>
2024-02-27 13:53:34 +0300

    yazım hatası giderildi.

--- Models/Dandik.cs
+++ Models/Dandik.cs
@@ -1,7 +1,7 @@
 namespace bulgarita.Models;
 public class Dandik
 {
-    public string? Name {get;set;}
+    public string Name {get;set;}
     public int DandikSeviyesi {get;set;}
     public Dandik(string name, int seviye)
     {
