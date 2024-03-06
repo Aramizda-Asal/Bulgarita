84c4dfd1c934b4991329ec34912589d65a214490
güneş balcı <gunesbalci66@hotmail.com>
2024-03-06 18:18:46 +0300

    servisler bir önceki commitde eklenmemişti, şimdi eklendi

---
+++ Data/Bağlantı.cs
@@ -0,0 +1,11 @@
+using System;
+using System.IO;
+using MySql.Data.MySqlClient;
+
+namespace bulgarita;
+
+internal class Bağlantı
+{
+    internal static string[] MySQL = File.ReadAllLines("VT1.txt");
+    internal static string bağlantı_dizisi = $"server={MySQL[0]};port={MySQL[1]};userid={MySQL[2]};password={MySQL[3]};database={MySQL[4]}";     
+}
