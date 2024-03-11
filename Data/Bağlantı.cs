18212bbfa3ebb8b75c50d0ae5ef008b95d73a544
Habil Tatarogullari <habiltatar01@gmail.com>
2024-03-11 14:43:16 +0300

    Ayarlar dosyasının yolu düzenlendi.

--- Data/Bağlantı.cs
+++ Data/Bağlantı.cs
@@ -6,6 +6,6 @@ namespace bulgarita;
 
 internal class Bağlantı
 {
-    internal static string[] MySQL = File.ReadAllLines("VT1.txt");
+    internal static string[] MySQL = File.ReadAllLines("Ayarlar/VT1.txt");
     internal static string bağlantı_dizisi = $"server={MySQL[0]};port={MySQL[1]};userid={MySQL[2]};password={MySQL[3]};database={MySQL[4]}";     
 }
