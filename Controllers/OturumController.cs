7c1985de8401abd2427dc55479427a88274a3dbd
Yusuf Kozan <kozanyusuf@outlook.com.tr>
2024-03-12 17:24:30 +0300

    OturumController'a başlandı.

---
+++ Controllers/OturumController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+using bulgarita.Models;
+using bulgarita.Services;
+
+namespace bulgarita.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+
+public class Oturum : ControllerBase
+{}
--- Services/Bağlantı.cs
+++ Services/Bağlantı.cs
@@ -2,7 +2,7 @@ using System;
 using System.IO;
 using MySql.Data.MySqlClient;
 
-namespace bulgarita;
+namespace bulgarita.Services;
 
 internal class Bağlantı
 {
