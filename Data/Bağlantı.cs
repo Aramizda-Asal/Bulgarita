91bfb7070a074e94a28ac235a4f9ca6ed15ccd9b
Habil Tatarogullari <habiltatar01@gmail.com>
2024-03-12 13:10:09 +0300

    Bağlantı'ya tablo isimleri eklendi.

--- Data/Bağlantı.cs
+++ Data/Bağlantı.cs
@@ -8,4 +8,13 @@ internal class Bağlantı
 {
     internal static string[] MySQL = File.ReadAllLines("Ayarlar/VT1.txt");
     internal static string bağlantı_dizisi = $"server={MySQL[0]};port={MySQL[1]};userid={MySQL[2]};password={MySQL[3]};database={MySQL[4]}";     
+
+    internal static string[] tablolar = File.ReadAllLines("Ayarlar/VT2.txt");
+
+    internal static string Kullanıcı_Tablosu = tablolar[0];
+    internal static string Oturum_Tablosu = tablolar[1];
+    internal static string Harita_Tablosu = tablolar[2];
+    internal static string Favoriler_Tablosu = tablolar[3];
+    internal static string Bilgi_Doğrulama_Tablosu = tablolar[4];
 }
+ 
