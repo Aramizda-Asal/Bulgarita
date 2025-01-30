e5f10a724113add7d0100c48e9b7b117a4125990
Güneş <gunesbalci66@hotmail.com>
2025-01-30 14:52:09 +0300

    RolController güncellendi. Her role özel controller eklendi.

--- Controllers/RolController.cs
+++ Controllers/RolController.cs
@@ -8,10 +8,10 @@ namespace bulgarita.Controllers;
 
 public class Roller : ControllerBase
 {
-    [HttpPost("RolVer/{Kullanıcı_Kimliği}/{Rol}")]
-    public IActionResult RolVer(string Kullanıcı_Kimliği, string Rol)
+    [HttpPost("RolVer_NoktaEkleyici/{Kullanıcı_Kimliği}")]
+    public IActionResult RolVer_NoktaEkleyici(string Kullanıcı_Kimliği)
     {
-        Models.Roller roller = new Models.Roller(Kullanıcı_Kimliği, Rol);
+        Models.Roller roller = new Models.Roller(Kullanıcı_Kimliği, "Nokta Ekleyici");
 
         if(RollerFonksiyonları.RolVer(roller))
         {
@@ -23,10 +23,130 @@ public class Roller : ControllerBase
         }
     }
 
-    [HttpDelete("RolAl/{Kullanıcı_Kimliği}/{Rol}")]
-    public IActionResult RolAl(string Kullanıcı_Kimliği, string Rol)
+    [HttpPost("RolVer_NoktaDüzenleyici/{Kullanıcı_Kimliği}")]
+    public IActionResult RolVer_NoktaDüzenleyici(string Kullanıcı_Kimliği)
     {
-        Models.Roller roller = new Models.Roller(Kullanıcı_Kimliği, Rol);
+        Models.Roller roller = new Models.Roller(Kullanıcı_Kimliği, "Nokta Düzenleyici");
+
+        if(RollerFonksiyonları.RolVer(roller))
+        {
+            return new StatusCodeResult(201); //Created
+        }
+        else
+        {
+            return new StatusCodeResult(422); //Unprocessable Content
+        }
+    }
+
+    [HttpPost("RolVer_NoktaSilici/{Kullanıcı_Kimliği}")]
+    public IActionResult RolVer_NoktaSilici(string Kullanıcı_Kimliği)
+    {
+        Models.Roller roller = new Models.Roller(Kullanıcı_Kimliği, "Nokta Silici");
+
+        if(RollerFonksiyonları.RolVer(roller))
+        {
+            return new StatusCodeResult(201); //Created
+        }
+        else
+        {
+            return new StatusCodeResult(422); //Unprocessable Content
+        }
+    }
+
+    [HttpPost("RolVer_RolAtayıcıAlıcı/{Kullanıcı_Kimliği}")]
+    public IActionResult RolVer_RolAtayıcıAlıcı(string Kullanıcı_Kimliği)
+    {
+        Models.Roller roller = new Models.Roller(Kullanıcı_Kimliği, "Rol Atayıcı/Alıcı");
+
+        if(RollerFonksiyonları.RolVer(roller))
+        {
+            return new StatusCodeResult(201); //Created
+        }
+        else
+        {
+            return new StatusCodeResult(422); //Unprocessable Content
+        }
+    }
+
+    [HttpPost("RolVer_KullanıcıSilici/{Kullanıcı_Kimliği}")]
+    public IActionResult RolVer_KullanıcıSilici(string Kullanıcı_Kimliği)
+    {
+        Models.Roller roller = new Models.Roller(Kullanıcı_Kimliği, "Kullanıcı Silici");
+
+        if(RollerFonksiyonları.RolVer(roller))
+        {
+            return new StatusCodeResult(201); //Created
+        }
+        else
+        {
+            return new StatusCodeResult(422); //Unprocessable Content
+        }
+    }
+
+    [HttpDelete("RolAl_NoktaEkleyici/{Kullanıcı_Kimliği}")]
+    public IActionResult RolAl_NoktaEkleyici(string Kullanıcı_Kimliği)
+    {
+        Models.Roller roller = new Models.Roller(Kullanıcı_Kimliği, "Nokta Ekleyici");
+
+        if(RollerFonksiyonları.RolAl(roller))
+        {
+            return new StatusCodeResult(200); //OK
+        }
+        else
+        {
+            return new StatusCodeResult(422); //Unprocessable Content
+        }
+    }
+
+    [HttpDelete("RolAl_NoktaDüzenleyici/{Kullanıcı_Kimliği}")]
+    public IActionResult RolAl_NoktaDüzenleyici(string Kullanıcı_Kimliği)
+    {
+        Models.Roller roller = new Models.Roller(Kullanıcı_Kimliği, "Nokta Düzenleyici");
+
+        if(RollerFonksiyonları.RolAl(roller))
+        {
+            return new StatusCodeResult(200); //OK
+        }
+        else
+        {
+            return new StatusCodeResult(422); //Unprocessable Content
+        }
+    }
+
+    [HttpDelete("RolAl_NoktaSilici/{Kullanıcı_Kimliği}")]
+    public IActionResult RolAl_NoktaSilici(string Kullanıcı_Kimliği)
+    {
+        Models.Roller roller = new Models.Roller(Kullanıcı_Kimliği, "Nokta Silici");
+
+        if(RollerFonksiyonları.RolAl(roller))
+        {
+            return new StatusCodeResult(200); //OK
+        }
+        else
+        {
+            return new StatusCodeResult(422); //Unprocessable Content
+        }
+    }
+
+    [HttpDelete("RolAl_RolAtayıcıAlıcı/{Kullanıcı_Kimliği}")]
+    public IActionResult RolAl_RolAtayıcıAlıcı(string Kullanıcı_Kimliği)
+    {
+        Models.Roller roller = new Models.Roller(Kullanıcı_Kimliği, "Rol Atayıcı/Alıcı");
+
+        if(RollerFonksiyonları.RolAl(roller))
+        {
+            return new StatusCodeResult(200); //OK
+        }
+        else
+        {
+            return new StatusCodeResult(422); //Unprocessable Content
+        }
+    }
+
+    [HttpDelete("RolAl_KullanıcıSilici/{Kullanıcı_Kimliği}")]
+    public IActionResult RolAl_KullanıcıSilici(string Kullanıcı_Kimliği)
+    {
+        Models.Roller roller = new Models.Roller(Kullanıcı_Kimliği, "Kullanıcı Silici");
 
         if(RollerFonksiyonları.RolAl(roller))
         {
