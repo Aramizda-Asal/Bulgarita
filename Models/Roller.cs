c3a821f43f8e5550f271aa04f88f8febf29d7b10
Güneş <gunesbalci66@hotmail.com>
2025-02-23 20:36:32 +0300

    Rol modeline mevcut rolleri bulunduran bir dizi eklendi.

--- Models/Roller.cs
+++ Models/Roller.cs
@@ -1,10 +1,21 @@
+using System.Reflection.Metadata;
+
 namespace bulgarita.Models;
 
 public class Roller
 {
+    public static readonly string[] RollerDizisi = ["Nokta Ekleyici",
+        "Nokta Düzenleyici","Nokta Silici","Rol Atayıcı/Alıcı","Kullanıcı Silici"];
+    public enum RollerE
+    {
+        NoktaEkleyici,
+        NoktaDüzenleyici,
+        NoktaSilici,
+        RolAtayıcıAlıcı,
+        KullanıcıSilici
+    }
     public string Kullanıcı;
     public string Rol;
-
     public Roller(string Kullanıcı, string Rol)
     {
         this.Kullanıcı = Kullanıcı;
