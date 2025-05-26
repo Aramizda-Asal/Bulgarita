2da69438e7074a282dec8a1bcaad1e9e6c62643e
Yusuf Kozan <kozanyusuf@outlook.com.tr>
2025-05-26 20:40:36 +0300

    Program GNU AGPL 3+ koşullarıyla lisanslanmıştır.

--- Program.cs
+++ Program.cs
@@ -1,23 +1,39 @@
 /*
  * Copyright (C) 2025 Güneş Balcı, Habil Tataroğulları, Yusuf Kozan
- *
- * Bu belge aşağıdaki telif hakkı ve izin bildirimince kapsanan çalışma
- * ya da çalışmalar içerir:
- *
+ * 
+ * This file is part of "Bulgaristan’da Harita Bazlı Yer İsimleri Uygulaması".
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * 
+ * 
+ * This file incorporates work that has been covered by the copyright
+ * and permission notice below:
+ * 
  *      Copyright (c) .NET Foundation and Contributors
- *
+ * 
  *      All rights reserved.
- *
+ * 
  *      Permission is hereby granted, free of charge, to any person obtaining a copy
  *      of this software and associated documentation files (the "Software"), to deal
  *      in the Software without restriction, including without limitation the rights
  *      to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  *      copies of the Software, and to permit persons to whom the Software is
  *      furnished to do so, subject to the following conditions:
- *
+ * 
  *      The above copyright notice and this permission notice shall be included in all
  *      copies or substantial portions of the Software.
- *
+ * 
  *      THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  *      IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  *      FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -25,10 +41,11 @@
  *      LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  *      OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  *      SOFTWARE.
- *
- * Bu belgenin temelini oluşturan çalışmaya
+ * 
+ * 
+ * You can access the work that forms the basis of this file through this link:
  * https://github.com/dotnet/aspnetcore/blob/v8.0.2/src/ProjectTemplates/Web.ProjectTemplates/content/WebApi-CSharp/Program.cs
- * adresinden erişebilirsiniz.
+ *
  */
 
 var builder = WebApplication.CreateBuilder(args);
--- README.md
+++ README.md
@@ -1,4 +1,19 @@
 # Bulgarita Art Uç
 Bulgaristan’da Harita Bazlı Yer İsimleri Uygulaması: Türkçe - Bulgarca projesinin art uç kodları
 
+---
+
 *Copyright (C) 2025 Habil Tataroğulları, Güneş Balcı, Yusuf Kozan*
+
+*This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.*
+
+*This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.*
+
+*You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.*
